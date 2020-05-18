e20c74afbe42ad4eb684bced9f32a974e0b761eb
sdass <sdass@drf.com>
2020-05-18 18:14:33 -0400

    additions works with rest get.

---
+++ src/main/java/com/subra/CalcController.java
@@ -0,0 +1,22 @@
+package com.subra;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class CalcController {
+	
+	/* 
+	 C:\Users\sdass>curl http://localhost:8082/5/6
+	11
+	*/
+	@GetMapping("/{a}/{b}")
+	Integer add(@PathVariable("a") Integer a, @PathVariable("b") Integer b){
+		System.out.println("received=" + a + " + "  + b);
+		int result = a+b;
+		System.out.println("returned=" +  result);
+		return result;
+	}
+
+}
