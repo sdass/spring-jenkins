8d69f0d6ab1a287b880fa49a2e5136345abc9d81
sdass <sdass@drf.com>
2020-05-18 19:54:22 -0400

    2 endpoints

--- src/main/java/com/subra/CalcController.java
+++ src/main/java/com/subra/CalcController.java
@@ -4,17 +4,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/")
+@RestController //("/")
 public class CalcController {
 	
 	/* 
 	 C:\Users\sdass>curl http://localhost:8082/5/6
 	11
 	*/
-	@GetMapping("/{a}/{b}")
+	@GetMapping("add/{a}/{b}")
 	Integer add(@PathVariable("a") Integer a, @PathVariable("b") Integer b){
 		System.out.println("received=" + a + " + "  + b);
 		int result = a+b;
+		System.out.println("returned=" +  result);
+		return result;
+	}
+	
+	@GetMapping("minus/{a}/{b}")
+	Integer subtract(@PathVariable("a") Integer a, @PathVariable("b") Integer b){
+		System.out.println("received=" + a + " - "  + b);
+		int result = a-b;
 		System.out.println("returned=" +  result);
 		return result;
 	}
